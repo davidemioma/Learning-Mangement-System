947b9d4afe9450960aa9b362ffcfb8e30d981c02
david emioma <davidemiomauche@gmail.com>
2023-10-05 17:22:37 +0100

    Built courses page

--- app/(dashboard)/(routes)/teacher/courses/page.tsx
+++ app/(dashboard)/(routes)/teacher/courses/page.tsx
@@ -1,12 +1,28 @@
-import Link from "next/link";
-import { Button } from "@/components/ui/button";
+import { auth } from "@clerk/nextjs";
+import prismadb from "@/lib/prismadb";
+import { redirect } from "next/navigation";
+import { columns } from "./_components/Columns";
+import { DataTable } from "./_components/DataTable";
 
-export default function Courses() {
+export default async function Courses() {
+  const { userId } = await auth();
+
+  if (!userId) {
+    return redirect("/");
+  }
+
+  const courses = await prismadb.course.findMany({
+    where: {
+      userId,
+    },
+    orderBy: {
+      createdAt: "desc",
+    },
+  });
+
   return (
     <div className="p-6">
-      <Link href="/teacher/create">
-        <Button>New Course</Button>
-      </Link>
+      <DataTable columns={columns} data={courses} />
     </div>
   );
 }
